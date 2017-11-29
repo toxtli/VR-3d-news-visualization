370885c9c52018b9ca5bb9fce0039a8fce836d96
Carlos Toxtli <ctoxtli@gmail.com>
2017-11-29 00:44:55 -0500

    Fullscreen button

--- js/index.js
+++ js/index.js
@@ -239,7 +239,7 @@ function initControls() {
 		}
 	});
 	$("#fullscreen").on("click", function(e) {
-		document.body.requestFullscreen();
+		toggleFullScreen();
 	});
 	$("#graph").on("click", function(e) {
 		$("#3d-graph").remove();
@@ -249,6 +249,21 @@ function initControls() {
 	$(".hideGraph").on("click", function(e) {
 		$("#3d-graph").remove();
 	});
+}
+
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
 }
 
 function enableNoSleep() {
